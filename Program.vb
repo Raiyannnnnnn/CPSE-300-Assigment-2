707f76e3d8d5477128d559b828d35f2a1f89d4e6
krishi-patel25 <patelk4@unbc.ca>
2025-11-15 00:35:59 -0800

    Update Program.vb

--- Program.vb
+++ Program.vb
@@ -1,24 +1,28 @@
 Imports System
-Imports System.Collections.Generic
+Imports System.Windows.Forms
 
 Module Program
-    Sub Main(args As String())
-        ' ====== Create sample customers ======
+    <STAThread>
+    Sub Main()
+        ' Uncomment the next line to run console tests instead of GUI
+        ' RunConsoleTests()
+        
+        Application.EnableVisualStyles()
+        Application.SetCompatibleTextRenderingDefault(False)
+        Application.Run(New Form1())
+    End Sub
+
+    Private Sub RunConsoleTests()
+        Console.WriteLine("=== Running Console Tests ===")
+        
         Dim customers As New List(Of Customer)()
-        customers.Add(New Customer(0, 5))   ' Arrives at time 0, needs 5 units of service
-        customers.Add(New Customer(2, 3))   ' Arrives at time 2, needs 3 units of service
-        customers.Add(New Customer(4, 2))   ' Arrives at time 4, needs 2 units of service
-        customers.Add(New Customer(6, 4))   ' Arrives at time 6, needs 4 units of service
-
-        ' ====== Create and run simulation ======
+        customers.Add(New Customer(0, 5))
+        customers.Add(New Customer(2, 3))
+        
         Dim simulation As New BankSimulation()
-        simulation.Initialize(customers)
-        simulation.Run()
-
-        ' ====== Display results ======
-        Console.WriteLine(simulation.GetFormattedOutput())
-
-        Console.WriteLine(vbCrLf & "Press any key to exit...")
+        Console.WriteLine(simulation.RunSimulation(customers))
+        
+        Console.WriteLine("Press any key to exit console mode...")
         Console.ReadKey()
     End Sub
 End Module
